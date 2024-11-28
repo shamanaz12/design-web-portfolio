ab555513310eb4f71498914496d4b7f9890f6f7a
Shama Naz <shama20302022@gmail.com>
2024-11-28 12:49:28 -0800

    First Commit-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   // Specify the files Tailwind should scan for class names
   content: [
-    './index.html',      // Home page
+    './index.html',      
     './about.html',      // About page
     './contact.html',    // Contact page
     './**/*.js',         // Any JS files (if you're using JS or Tailwind classes within JS)
